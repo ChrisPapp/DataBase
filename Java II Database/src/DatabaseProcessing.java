106e25ab08060bccd16112068cfe4dc68a3fc084
ElpinikiSeva <t8170116@dias.aueb.gr>
2018-10-19 21:47:06 +0300

    Add DatabaseProcessing class

---
+++ Java II Database/src/DatabaseProcessing.java
@@ -0,0 +1,32 @@
+
+public class DatabaseProcessing {
+	//Process data from database
+	private Database data;
+	
+	public DatabaseProcessing(Database data) {
+		this.data = data;	
+	}
+
+	public Database getData() {
+		return data;
+	}
+
+	public void setData(Database data) {
+		this.data = data;
+	}
+	
+	public void dataInput(Database data) {
+	
+		
+	}
+	
+	public void dataRemove(Database data) {
+		
+		
+	}
+	
+	public void dataChange(Database data) {
+		
+		
+	}
+}
