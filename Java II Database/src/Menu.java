b7204c14321fb7ac080af1b97bd052c96c2bef78
ElpinikiSeva <t8170116@dias.aueb.gr>
2018-10-19 21:12:19 +0300

    Add Menu class

---
+++ Java II Database/src/Menu.java
@@ -0,0 +1,25 @@
+
+public class Menu {
+	//Print main menu and run user's choice
+	private Database data;
+	
+	public Menu(Database data) {
+		this.data = data;
+	}
+
+	public Database getData() {
+		return data;
+	}
+
+	public void setData(Database data) {
+		this.data = data;
+	}
+	
+	public void printAndChoose() {
+
+		
+		
+		
+	}
+
+}
