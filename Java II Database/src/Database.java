424e2c699296a84373dc62f5a34e8a7866d158c0
ElpinikiSeva <t8170116@dias.aueb.gr>
2018-10-19 20:44:54 +0300

    Add Database class

---
+++ Java II Database/src/Database.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Database {
+	//Fill in our Database with fields and data
+	private ArrayList<String> fields = new ArrayList<String>();
+	private ArrayList<ArrayList<Object>> list = new ArrayList<ArrayList<Object>>();
+	
+	public Database() {
+		
+		
+	}
+
+	public ArrayList<String> getFields() {
+		return fields;
+	}
+
+	public void setFields(ArrayList<String> fields) {
+		this.fields = fields;
+	}
+
+	public ArrayList<ArrayList<Object>> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<ArrayList<Object>> list) {
+		this.list = list;
+	}
+	
+	public void inputFields() {
+		
+		
+	}
+	
+	public void inputData() {
+		
+		
+	}
+}
