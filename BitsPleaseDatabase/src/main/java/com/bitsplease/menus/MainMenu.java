0c9008100d8accf25a066289a4e8edca3f9c04d8
ChrisPapp <chrispappas99@yahoo.gr>
2018-11-21 03:46:40 +0200

    Second Presentation Version

--- BitsPleaseDatabase/src/main/java/com/bitsplease/menus/MainMenu.java
+++ BitsPleaseDatabase/src/main/java/com/bitsplease/menus/MainMenu.java
@@ -40,9 +40,10 @@ public class MainMenu extends AbstractMenu {
     switch (choice) {
     case 1:
       Scanner obj = new Scanner(System.in);
-      System.out.println("IF YOU WANT TO SEE WHOLE DATA NAMES PRESS 1 OR ELSE PRESS 2");
+      System.out.println(
+          "IF YOU WANT TO SEE WHOLE DATA NAMES PRESS 1 OR ELSE PRESS 2");
       int press = obj.nextInt();
-        DatabaseProcessing.displayData(data.getList(), press);
+      DatabaseProcessing.displayData(data.getList(), press);
       break;
     case 2:
       data.inputData();
@@ -57,8 +58,12 @@ public class MainMenu extends AbstractMenu {
       DatabaseProcessing.removeLine(data.getList());
       break;
     case 5:
+      DatabaseProcessing.removeColumn(data.getList());
+      break;
+    case 6:
       System.out.println(" SELECT COLUMN: ");
-      int askedColumn = inputChoice.nextInt() - 1;
+      Scanner inputColumn = new Scanner(System.in);
+      int askedColumn = DatabaseProcessing.codeToNum(inputColumn.nextLine());
       if (DataProcessingMore.areAllNumbers(data.getList(), askedColumn)) {
         DataProcessingMore.sumOfAll(data.getList(), askedColumn);
         DataProcessingMore.productOfAll(data.getList(), askedColumn);
@@ -69,36 +74,15 @@ public class MainMenu extends AbstractMenu {
             "Check your data and change the ones that are not numbers!");
       }
       break;
-    case 6:
+    case 7:
       System.out.println(" GIVE OPERATION ");
       String askedOperation = inputOperation.nextLine();
       DataProcessingMore.extraordinaryOption(data.getList(), askedOperation);
       break;
-    case 7:
-      DatabaseProcessing.rotate(data.getList());// Line mode
-      System.out.print("WHICH LINE DO YOU WANT TO SEE? (1 - "
-          + data.getList().size() + ") ");
-      Scanner sc = new Scanner(System.in);
-      int line = sc.nextInt() - 1;
-      System.out.println();
-      DatabaseProcessing.printLine(data.getList(), line);
-      DatabaseProcessing.rotate(data.getList()); // Column mode
-      break;
     case 8:
-      System.out.print("WHICH COLUMN DO YOU WANT TO SEE? (A - "
-          + DatabaseProcessing.numToCode(data.getList().get(0).size() - 1)
-          + ") ");
-      Scanner in = new Scanner(System.in);
-      String column = in.nextLine();
-      System.out.println();
-      System.out.println(column);
-      DatabaseProcessing.printColumn(
-          data.getList().get(DatabaseProcessing.codeToNum(column)));
-      break;
-    case 9:
       search();
       break;
-    case 0:
+    case 9:
       sort();
       break;
     default:
@@ -138,8 +122,8 @@ public class MainMenu extends AbstractMenu {
   protected final void printMenu() {
     System.out.println("\n*** DATABASE MENU ***\n" + "  1. DISPLAY DATA \n"
         + "  2. INPUT DATA \n" + "  3. CHANGE DATA\n" + "  4. REMOVE LINE\n"
-        + "  5. MORE CALCULATIONS \n" + "  6. OPERATIONS BETWEEN COLUMNS \n"
-        + "  7. PRINT A LINE \n" + "  8. PRINT A COLUMN \n" + "  9. SEARCH \n"
-        + "  0. SORT \n" + "  SELECT AN OPTION: ");
+        + "  5. REMOVE COLUMN\n" + "  6. MORE CALCULATIONS \n"
+        + "  7. OPERATIONS BETWEEN COLUMNS \n" + "  8. SEARCH \n"
+        + "  9. SORT \n" + "  SELECT AN OPTION: ");
   }
 }
